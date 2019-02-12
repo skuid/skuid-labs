94c779d21bcfed3588a062d4706d2f272f5eeb7c
Jer Carpenter <jer.carpenter@skuid.com>
2019-02-12 17:35:54 -0500

    Fixed TimeAdd snippet

--- Custom Field Renderers/Time Entry/CustomFieldRenderer-Time.js
+++ Custom Field Renderers/Time Entry/CustomFieldRenderer-Time.js
@@ -1,11 +1,14 @@
-//Field editor for time entry
+var field = arguments[0],
+    value = arguments[1],
+    $ = skuid.$;
+
 switch(field.mode) {
     case 'edit':
-        skuid.ui.fieldRenderers['TEXT'].edit(field, value);
+        skuid.ui.fieldRenderers.TEXT.edit(field, value);
         field.element.find("input").mask('00:00:00', {reverse: true});
         break;
 
     default:
-        skuid.ui.fieldRenderers[field.displaytype].[field.mode](field, value);
+        skuid.ui.fieldRenderers[field.displaytype][field.mode](field, value);
 	break;
 }
