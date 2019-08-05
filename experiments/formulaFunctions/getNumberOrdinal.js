ffabcce1ae481e243ed1a250d19efb39c6446b6b
Elena Hatch <elena@skuid.com>
2019-08-05 13:00:54 -0400

    Update getNumberOrdinal.js

--- experiments/formulaFunctions/getNumberOrdinal.js
+++ experiments/formulaFunctions/getNumberOrdinal.js
@@ -1,17 +1,17 @@
 //Returns the correct ordinal suffix for a number
 //ex. 1st, 2nd, 3rd etc. 
 skuid.formula.Formula (
-  'GET_ORDINAL',
-  function (num) {
-  	if (num > 3 && num < 21) return 'th'; 
-  	switch (num % 10) {
-    	case 1:  return "st";
-    	case 2:  return "nd";
-    	case 3:  return "rd";
-    	default: return "th";
-  	}
-  },{
-    numArgs : 1,
-    returnType : 'string'
-  }
+	'GET_ORDINAL',
+	function (num) {
+		if (num > 3 && num < 21) return 'th'; 
+		switch (num % 10) {
+			case 1:  return "st";
+			case 2:  return "nd";
+			case 3:  return "rd";
+			default: return "th";
+		}
+	},{
+		numArgs : 1,
+		returnType : 'string'
+	}
 );
