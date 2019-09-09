7c47f9058023202091e183e45b8cfac5a5b502a7
Jer Carpenter <jer.carpenter@skuid.com>
2019-09-09 17:02:58 -0400

    Added actionFrameworkDebugging.js

---
+++ experiments/snippets/debugging/actionFramework/actionFrameworkDebugging.js
@@ -0,0 +1,34 @@
+/*
+Action logging in Skuid! This adds console output for all actions called during page runtime.
+Paste this into an 'Inline' or include it as a page resource.
+To enable, add 'actionloglevel=1' or 'actionloglevel=2' to the page url parameters.
+Special thanks to Zach McElrath for help finding where and how to override and extend the Skuid action function.
+BIG DISCLAIMER: No guarantee this will continue to work through future Skuid upgrades, since it overrides a core function,
+and does not use any documented or supported Skuid JS API stuff.
+ */
+if(skuid.page.params.actionloglevel) {
+	var original = skuid.actions.runAction,
+		$ = skuid.$,
+		loglevel = parseInt(skuid.page.params.actionloglevel);
+	skuid.lastActionTime = performance.now(); //Kinda helps with showing action durations, but... async, so... not reliable.
+	if(loglevel > 2 || loglevel < 1) console.warn("Invalid actionlog level " + loglevel);
+	skuid.actions.runAction = function(actionType, c, f, context) {
+		var result = original.apply(this, arguments);
+		var perfTime = "Skuid Action started after " + (performance.now() - skuid.lastActionTime).toFixed(4) + "ms";
+		switch(actionType) {
+			default:
+				switch(loglevel) {
+					case 1:
+						console.log(perfTime, $.extend(true, {}, c[0]));
+						break;
+					case 2:
+						console.log(perfTime, $.extend(true, {}, c[0]), arguments);
+						break;
+				}
+				break;
+		}
+		skuid.lastActionTime = performance.now();
+		return result;
+	};
+}
+
