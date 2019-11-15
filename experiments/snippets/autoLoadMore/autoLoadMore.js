ee04c4db2baebe3d6a48d20209a4d6c16c89eb4d
mackinaw <paul@leancog.com>
2019-11-15 14:57:34 -0800

    Update experiments/snippets/autoLoadMore/autoLoadMore.js
    Co-Authored-By: Cody Taylor <itmightbecody@users.noreply.github.com>

--- experiments/snippets/autoLoadMore/autoLoadMore.js
+++ experiments/snippets/autoLoadMore/autoLoadMore.js
@@ -4,7 +4,7 @@ var $ = skuid.$;
 
 $.fn.isInViewport = function() {
 	var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
+	var elementBottom = elementTop + $(this).outerHeight();
     var viewportTop = $(window).scrollTop();
     var viewportBottom = viewportTop + $(window).height();
     return elementBottom > viewportTop && elementTop < viewportBottom;
