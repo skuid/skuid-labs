cee166533783eded19bd28d7e320e8931c229da0
Cody Taylor <cody@skuid.com>
2023-09-25 11:08:42 -0400

    Clean up controller code + add some comments

--- experiments/pages/lightningScreenFlowInSkuid/force-app/main/default/aura/showScreenFlowModal/showScreenFlowModalController.js
+++ experiments/pages/lightningScreenFlowInSkuid/force-app/main/default/aura/showScreenFlowModal/showScreenFlowModalController.js
@@ -1,17 +1,16 @@
 ({
 	handleShowModalFlowEvent: function(component, event, helper) {
         let eventName = event.getParam("name");
-		// Check that we've got a request to show a modal
-		// If not, return. Otherwise, start setting our variables.
+		// Verify that the event is for showing the modal
         if (eventName != 'showModalFlow') return;
+        // Get our flow name and variable values
         let data = event.getParam("data");
 		let flowName = data.flowName;
         let variables = data.variables;
-        //let variables = [{name:"Name",type:"String",value:"HI"},{name:"OtherVariable",type:"Number",value:5}];
         component.set("v.showModal", true);		
-		// With our modal now showing, we can access the flow
+		// With our modal now showing, we can access and start the flow
         let flow = component.find("flow");
-            flow.startFlow(flowName,variables);
+        flow.startFlow(flowName,variables);
     },
     handleFlowStatusChange: function(component, event, helper) {
         if (event.getParam('status') === 'FINISHED') {
@@ -23,6 +22,7 @@
         component.set("v.showModal", false);
     },
     refreshPage: function(component, event, helper) {
+        // Fire an event that can be listened to by Skuid for post-modal actions
         var evt = $A.get("e.skuid:event");
         evt.setParams({"name": "flowModalClosed"});
         evt.fire();
