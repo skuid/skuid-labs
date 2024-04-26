c398dcc17ae67ed96c85b50cb9b5bc1188f55601
cjstern <63868385+cjstern@users.noreply.github.com>
2024-04-26 13:51:28 -0400

    Update showScreenFlowModalController.js
    Updates JS to publish unique events for close and cancel that Skuid can consume

--- experiments/pages/lightningScreenFlowInSkuid/force-app/main/default/aura/showScreenFlowModal/showScreenFlowModalController.js
+++ experiments/pages/lightningScreenFlowInSkuid/force-app/main/default/aura/showScreenFlowModal/showScreenFlowModalController.js
@@ -1,30 +1,35 @@
 ({
 	handleShowModalFlowEvent: function(component, event, helper) {
-        let eventName = event.getParam("name");
-		// Verify that the event is for showing the modal
-        if (eventName != 'showModalFlow') return;
-        // Get our flow name and variable values
+        let name = event.getParam("name");
+        if (name != 'showModalFlow') return;
         let data = event.getParam("data");
-		let flowName = data.flowName;
-        let variables = data.variables;
-        component.set("v.showModal", true);		
-		// With our modal now showing, we can access and start the flow
+
+        component.set("v.showModal", true);
         let flow = component.find("flow");
-        flow.startFlow(flowName,variables);
+        flow.startFlow(data.flowName, data && data.params ? data.params : null);
     },
     handleFlowStatusChange: function(component, event, helper) {
         if (event.getParam('status') === 'FINISHED') {
-            var action = component.get("c.refreshPage");
+            var action = component.get("c.closeModal");
             $A.enqueueAction(action);
         }
     },
     handleClose: function (component, event, helper) {
-        component.set("v.showModal", false);
+		var action = component.get("c.cancelModal");        
+        $A.enqueueAction(action);
     },
-    refreshPage: function(component, event, helper) {
+    closeModal: function(component, event, helper) {
         // Fire an event that can be listened to by Skuid for post-modal actions
+        component.set("v.showModal", false);
         var evt = $A.get("e.skuid:event");
         evt.setParams({"name": "flowModalClosed"});
         evt.fire();
-    }
-})
+    },	
+    cancelModal: function(component, event, helper) {
+        // Fire an event that can be listened to by Skuid for post-modal actions
+        component.set("v.showModal", false);
+        var evt = $A.get("e.skuid:event");
+        evt.setParams({"name": "flowModalCancelled"});
+        evt.fire();
+    },	
+ })
