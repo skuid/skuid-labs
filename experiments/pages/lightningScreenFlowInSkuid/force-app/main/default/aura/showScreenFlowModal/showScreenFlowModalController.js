6744d08fc814a34fd3d713ccf7091db3cf5615a7
cjstern <63868385+cjstern@users.noreply.github.com>
2024-05-14 13:49:18 -0400

    Updating cmp and js screenflow (#95)
    * Update showScreenFlowModal.cmp
    
    Necessary component file updates
    
    * Update showScreenFlowModalController.js
    
    Updates JS to publish unique events for close and cancel that Skuid can consume
    
    * Update showScreenFlowModalController.js
    
    Updated top level node to work with Skuid page JS snippet
    
    * Update README.md
    
    Added troubleshooting note regarding the importance of having the showScreenFlowModal component present in the build canvas

--- experiments/pages/lightningScreenFlowInSkuid/force-app/main/default/aura/showScreenFlowModal/showScreenFlowModalController.js
+++ experiments/pages/lightningScreenFlowInSkuid/force-app/main/default/aura/showScreenFlowModal/showScreenFlowModalController.js
@@ -14,17 +14,26 @@
     },
     handleFlowStatusChange: function(component, event, helper) {
         if (event.getParam('status') === 'FINISHED') {
-            var action = component.get("c.refreshPage");
+            var action = component.get("c.closeModal");
             $A.enqueueAction(action);
         }
     },
     handleClose: function (component, event, helper) {
-        component.set("v.showModal", false);
+		var action = component.get("c.cancelModal");        
+        $A.enqueueAction(action);
     },
-    refreshPage: function(component, event, helper) {
+    closeModal: function(component, event, helper) {
         // Fire an event that can be listened to by Skuid for post-modal actions
+        component.set("v.showModal", false);
         var evt = $A.get("e.skuid:event");
         evt.setParams({"name": "flowModalClosed"});
         evt.fire();
-    }
-})
+    },	
+    cancelModal: function(component, event, helper) {
+        // Fire an event that can be listened to by Skuid for post-modal actions
+        component.set("v.showModal", false);
+        var evt = $A.get("e.skuid:event");
+        evt.setParams({"name": "flowModalCancelled"});
+        evt.fire();
+    },	
+ })
